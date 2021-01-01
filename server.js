2e7935f5011adb3f67ff608215fef3423a122ed6
Shawn McPhee <scmcphee9@gmail.com>
2020-12-31 20:03:22 -0700

    moved mysql connection information from server to connection under the config directory

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
+
 
 const app = express();
 
@@ -14,21 +14,9 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "skagwayak9",
-  database: "",
-});
 
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
+
+
 
 app.listen(PORT, () =>
   console.log(`Server listening on: http://localhost:${PORT}`)
