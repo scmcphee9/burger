658312b7a50201c15689e79a51e05a5d65713c4f
Shawn McPhee <scmcphee9@gmail.com>
2020-12-31 20:06:27 -0700

    moved server connection.connect to server.js

--- config/connection.js
+++ config/connection.js
@@ -8,12 +8,6 @@ const connection = mysql.createConnection({
   database: "burgers_db",
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
+
 
 module.exports = { connection };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-
+const connection = require("./connection");
 
 const app = express();
 
@@ -14,9 +14,13 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-
-
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
 
 app.listen(PORT, () =>
   console.log(`Server listening on: http://localhost:${PORT}`)
